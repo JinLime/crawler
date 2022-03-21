7755520e0d31a967125807d169e2938b553318e7
sj <bsj6551@naver.com>
2022-03-21 15:35:13 +0900

    puppeteer crawler

--- puppeteer.js
+++ puppeteer.js
@@ -1,16 +1,16 @@
-const parse = require("csv-parse/sync");
-const fs = require("fs");
 const puppeteer = require("puppeteer");
 
-const csv = fs.readFileSync("csv/data.csv");
-const records = parse.toString("utf-8");
-
 const crawler = async () => {
+    // headless: true = 화면 안보임(배보용), false = 화면 보임(개발용)
     const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto("https://www.google.com");
-    await page.waitFor(3000);
-    await page.close();
+    const [page1, page2] = await Promise.all([browser.newPage(), browser.newPage()]);
+
+    await Promise.all([page1.goto("https://www.naver.com/"), page2.goto("https://www.google.com")]);
+
+    await Promise.all([page1.waitForTimeout(3000), page2.waitForTimeout(5000)]);
+
+    await page1.close();
+    await page2.close();
     await browser.close();
 };
 
---
+++ puppeteer_crawler.js
@@ -0,0 +1,36 @@
+const { parse } = require("csv-parse/sync");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+
+const csv = fs.readFileSync("csv/data.csv");
+const records = parse(csv.toString("utf-8"));
+
+const crawler = async () => {
+    try {
+        const browser = await puppeteer.launch({
+            headless: process.env.NODE_ENV === "production",
+        });
+        await Promise.all(
+            records.map(async (v, i) => {
+                try {
+                    const page = await browser.newPage();
+                    await page.goto(v[1]);
+                    const scoreEl = await page.$(".score.score_left .star_score");
+                    if (scoreEl) {
+                        const text = await page.evaluate((tag) => tag.textContent, scoreEl);
+                        console.log(v[0], "평점 :", text.trim());
+                    }
+                    await page.waitForTimeout(5000);
+                    await page.close();
+                } catch (e) {
+                    console.error(e);
+                }
+            }),
+        );
+        await browser.close();
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+crawler();
