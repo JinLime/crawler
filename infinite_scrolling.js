9d4845df5e15f238a1684a35efdca23441dde6d8
sj <bsj6551@naver.com>
2022-03-29 15:14:16 +0900

    infinite scrolling

--- infinite_scrolling.js
+++ infinite_scrolling.js
@@ -1,4 +1,13 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
+const axios = require("axios");
+
+fs.readdir("imgs", (err) => {
+    if (err) {
+        console.error("imgs 폴더가 없어 imgs 폴더를 생성합니다.");
+        fs.mkdirSync("imgs");
+    }
+});
 
 const crawler = async () => {
     try {
@@ -6,7 +15,7 @@ const crawler = async () => {
         const page = await browser.newPage();
         await page.goto("https://unsplash.com");
         let result = [];
-        while (result.length <= 50) {
+        while (result.length <= 10) {
             const srcs = await page.evaluate(() => {
                 window.scrollTo(0, 0); // 절대 좌표, 스크롤 맨 위에 위치
                 const imgs = [];
@@ -26,11 +35,20 @@ const crawler = async () => {
                 return imgs;
             });
             result = result.concat(srcs);
+            // 다음 이미지가 로딩 될 때까지 대기
             await page.waitForSelector("figure");
             console.log("새 이미지 태그 로딩 완료!");
         }
-        console.log(result);
+        // console.log(result);
         console.log(result.length);
+        result.forEach(async (src) => {
+            // 이미지 링크를 Array Buffer 바꿈 (이미지링크 저장)
+            const imgResult = await axios.get(src.replace(/\?.*$/, ""), {
+                responseType: "arraybuffer",
+            });
+            fs.writeFileSync(`imgs/${new Date().valueOf()}.jpeg`, imgResult.data);
+            console.log("이미지 다운로드가 완료되었습니다.");
+        });
         await browser.close();
     } catch (e) {
         console.error(e);
