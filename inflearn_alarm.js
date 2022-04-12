f70678d9443bc24f50001b4af73178704dfbd427
DESKTOP-VE2KLNM\sj <bsj6551@gmail.com>
2022-04-13 00:40:59 +0900

    instargram

---
+++ inflearn_alarm.js
@@ -0,0 +1,4 @@
+const a = Array.from($$(".type-placeholder p span")).map((v) => v.textContent);
+const b = Array.from($$(".content__body.markdown-body p")).map((v) => v.textContent);
+
+//  https://api.telegram.org/bot5126219045:AAF85jT1nfJz7ZAvX_hzDT8sjv0W5HQyi68/getUpdates
--- instargram.js
+++ instargram.js
@@ -10,6 +10,8 @@ const crawler = async () => {
         const browser = await puppeteer.launch({
             headless: process.env.NODE_ENV === "production",
             args: ["--window-size=2560,1440", "--disable-notifications"],
+            // userDataDir: 유저 쿠키 가져오기 (로그인 저장됨)
+            userDataDir: "C:/Users/sj/AppData/Local/Google/Chrome/User Data/Default",
         });
         const page = await browser.newPage();
         await page.setViewport({
@@ -20,15 +22,19 @@ const crawler = async () => {
         await page.waitForResponse((response) => {
             return response.url().includes("logging_client_events");
         });
-        // page.waitForNavigation(); // React같은 싱글페이지에서 다른 페이지로 넘어가는 것을 기다림
-        await page.type("._9GP1n [name=username]", process.env.EMAIL);
-        await page.type("._9GP1n [name=password]", process.env.PASSWORD);
-        await page.waitForTimeout(2000);
-        await page.click(".qF0y9 [type=submit]");
-        await page.waitForResponse((response) => {
-            return response.url().includes("square_gradient_192.png/7c119b0c5722.png");
-        });
-        await page.click(".cmbtv");
+        if (await page.$('a[href="/jjjjinnnnn/"]')) console.log("이미 로그인 되어 있습니다.");
+        else {
+            page.waitForNavigation(); // React같은 싱글페이지에서 다른 페이지로 넘어가는 것을 기다림
+            await page.type("._9GP1n [name=username]", process.env.EMAIL);
+            await page.type("._9GP1n [name=password]", process.env.PASSWORD);
+            await page.waitForTimeout(2000);
+            await page.click(".qF0y9 [type=submit]");
+            await page.waitForResponse((response) => {
+                return response.url().includes("square_gradient_192.png/7c119b0c5722.png");
+            });
+            await page.click(".cmbtv");
+            console.log("로그인을 완료했습니다");
+        }
 
         // await page.close();
         // await browser.close();
